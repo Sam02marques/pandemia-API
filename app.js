609a237b68e5271fc7a2b2fd6ff7e9b982616ba2
samuel <marquessam02@gmail.com>
2025-07-02 11:10:59 -0300

    feat: Adiciona CORS para permitir requisições do Front-end

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // IMPORTA O EXPRESS PARA CRIAR O SERVIDOR
 import express from "express";
+import cors from 'cors';
 
 // IMPORTA AS ROTAS DE CADA RECURSO (PANDEMIA, FAKE NEWS E VACINAS)
 import rota_pandemia from './src/routes/pandemias.js';
@@ -9,7 +10,7 @@ import rota_vacina from './src/routes/vacinas.js';
 const app = express(); // CRIA A INSTÂNCIA DO SERVIDOR EXPRESS
 const porta = 5000; // DEFINE A PORTA EM QUE O SERVIDOR VAI ESCUTAR AS REQUISIÇÕES
 
-
+app.use(cors()); // USA O CORS ANTES DE TODAS AS ROTAS
 app.use(express.json()); // HABILITA O MIDDLEWARE PARA PARSER O CORPO DAS REQUISIÇÕES EM JSON (QUANDO FOR USADO MÉTODO POST SERÁ BASTANTE ÚTIL)
 
 // REGISTRA AS ROTAS PARA CADA TIPO DE RECURSO
